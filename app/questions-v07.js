d9afaf8f8aec1ea7233bb77373afef9f21c7b81b
quade1701Ac <quade1701@googlemail.com>
2026-08-27 14:35:35 +0200

    Activate varied LF5-12 replacement pool

--- app/questions-v07.js
+++ app/questions-v07.js
@@ -1,9 +1,10 @@
 // Zentraler Fragen-Aggregator.
-// LF 1–4: v05 + quality + v11 ergeben einen kuratierten 25er-Kern ohne den stark redundanten v04-Block.
-// LF 5–12: v08/v09 plus Qualitätsfragen.
+// LF 1–4: v05 + quality + v11 ergeben einen kuratierten Kern ohne den stark redundanten v04-Block.
+// LF 5–12: bestehender Grundstock plus neue praxis- und transferorientierte Qualitätsfragen.
 import { v08Questions } from './questions-v08';
 import { v09Questions } from './questions-v09';
 import { qualityQuestions } from './questions-quality';
 import { v11Questions } from './questions-v11';
+import { replacementLf5to12Questions } from './questions-replacement-lf5-12';
 
-export const v07Questions = [...v08Questions, ...v09Questions, ...qualityQuestions, ...v11Questions];
+export const v07Questions = [...v08Questions, ...v09Questions, ...qualityQuestions, ...v11Questions, ...replacementLf5to12Questions];
