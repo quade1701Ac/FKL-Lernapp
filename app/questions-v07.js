bc4e86b3fec02b2def57461b3448ad063541ef49
quade1701Ac <quade1701@googlemail.com>
2026-08-27 14:42:39 +0200

    Activate 48-question IHK-style expansion

--- app/questions-v07.js
+++ app/questions-v07.js
@@ -1,10 +1,10 @@
 // Zentraler Fragen-Aggregator.
-// LF 1–4: v05 + quality + v11 ergeben einen kuratierten Kern ohne den stark redundanten v04-Block.
-// LF 5–12: bestehender Grundstock plus neue praxis- und transferorientierte Qualitätsfragen.
+// Kuratierter Grundstock plus praxis-, transfer- und prüfungsorientierte Erweiterungen.
 import { v08Questions } from './questions-v08';
 import { v09Questions } from './questions-v09';
 import { qualityQuestions } from './questions-quality';
 import { v11Questions } from './questions-v11';
 import { replacementLf5to12Questions } from './questions-replacement-lf5-12';
+import { expansionV12Questions } from './questions-expansion-v12';
 
-export const v07Questions = [...v08Questions, ...v09Questions, ...qualityQuestions, ...v11Questions, ...replacementLf5to12Questions];
+export const v07Questions = [...v08Questions, ...v09Questions, ...qualityQuestions, ...v11Questions, ...replacementLf5to12Questions, ...expansionV12Questions];
