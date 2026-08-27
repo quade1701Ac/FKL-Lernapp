2ad97ac2609b599bc0514b1e06dfa9b6a7e69fa4
quade1701Ac <quade1701@googlemail.com>
2026-08-27 21:31:06 +0200

    Activate quality questions for learning fields 9 to 12

--- app/questions-v07.js
+++ app/questions-v07.js
@@ -7,5 +7,6 @@ import { v11Questions } from './questions-v11';
 import { replacementLf5to12Questions } from './questions-replacement-lf5-12';
 import { expansionV12Questions } from './questions-expansion-v12';
 import { qualityReplacementQuestions } from './question-quality-filter';
+import { qualityLf9to12Questions } from './questions-quality-lf9-12';
 
-export const v07Questions = [...v08Questions, ...v09Questions, ...qualityQuestions, ...v11Questions, ...replacementLf5to12Questions, ...expansionV12Questions, ...qualityReplacementQuestions];
+export const v07Questions = [...v08Questions, ...v09Questions, ...qualityQuestions, ...v11Questions, ...replacementLf5to12Questions, ...expansionV12Questions, ...qualityReplacementQuestions, ...qualityLf9to12Questions];
